1cb27ea22d68ffbfb3b775b4eaf7a75cfe89b976
wing chau <wing.s.chau@gmail.com>
2023-10-30 23:33:18 -0400

    Fix CMakeLists for unit tests, added skeletion code for now

---
+++ tests/main/SymbolicFactoryTest.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MODULE SymbolicFactoryTest
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(SymbolicFactory_test)
+{
+  int i = 1;
+  BOOST_TEST(i);
+
+}
--- tests/main/ref.cpp
+++ tests/main/ref.cpp
@@ -1,9 +1,9 @@
-#define BOOST_TEST_MODULE MyTest
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 
+// Reference code to ensure boost unit test works
 BOOST_AUTO_TEST_CASE(first_test)
 {
   int i = 1;
   BOOST_TEST(i);
-  BOOST_TEST(i == 2);
+  //BOOST_TEST(i == 2);
 }
